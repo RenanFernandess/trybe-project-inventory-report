58dee8ca7c7b49744f65ab90c747bb3b4759593d
Renan Fernandes <renzinestuods@gmail.com>
2023-04-05 16:53:28 -0300

    :sparkles: feat: Implementa o metodo import_data para gerar relatorio a partir de arquivos .csv e .json

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,18 @@
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class Inventory:
+    @classmethod
+    def import_data(cls, path: str, type: str = "simples"):
+        path_len = len(path)
+        if path[-4:path_len].__contains__(".csv"):
+            products = CsvImporter.import_data(path=path)
+        elif path[-5:path_len].__contains__(".json"):
+            products = JsonImporter.import_data(path=path)
+
+        if type == "completo":
+            return CompleteReport.generate(products)
+        return SimpleReport.generate(products)
