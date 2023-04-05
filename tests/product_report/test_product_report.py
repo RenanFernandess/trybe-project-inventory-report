74004e903a07b6a9972b1543a1ec44032abd3e4c
Renan Fernandes <renzinestuods@gmail.com>
2023-04-05 18:41:55 -0300

    :white_check_mark: test: Test that the report is generated in the correct format

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,23 @@
 # from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    """Test that the report is generated in the correct format"""
+    borracha = Product(
+        nome_do_produto="Borracha",
+        id=1,
+        nome_da_empresa="Papelaria Solar",
+        data_de_fabricacao="2021-07-04",
+        data_de_validade="2029-02-09",
+        numero_de_serie="FR48",
+        instrucoes_de_armazenamento="Ao abrigo de luz solar",
+    )
+
+    assert str(borracha) == (
+        "O produto Borracha"
+        + " fabricado em 2021-07-04"
+        + " por Papelaria Solar com validade"
+        + " até 2029-02-09"
+        + " precisa ser armazenado Ao abrigo de luz solar."
+    )
