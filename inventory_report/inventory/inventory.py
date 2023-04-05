54366f5167763adfdec5e0062bac8ec47ee57c53
Renan Fernandes <renzinestuods@gmail.com>
2023-04-05 18:27:04 -0300

    :sparkles: feat: Agora também é possivel gerar relatorio a partir de arquivos .xml

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 from inventory_report.importer.csv_importer import CsvImporter
 from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -8,10 +9,12 @@ class Inventory:
     @classmethod
     def import_data(cls, path: str, type: str = "simples"):
         path_len = len(path)
-        if path[-4:path_len].__contains__(".csv"):
+        if path[-4:path_len] == ".csv":
             products = CsvImporter.import_data(path=path)
-        elif path[-5:path_len].__contains__(".json"):
+        elif path[-5:path_len] == ".json":
             products = JsonImporter.import_data(path=path)
+        else:
+            products = XmlImporter.import_data(path=path)
 
         if type == "completo":
             return CompleteReport.generate(products)
