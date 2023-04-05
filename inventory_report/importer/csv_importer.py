ba357dcee2fea27440703cbac9cf817e22295984
Renan Fernandes <renzinestuods@gmail.com>
2023-04-05 18:29:16 -0300

    :sparkles: feat: Caso o arquivo não seja do formato correto lança uma exceção

--- inventory_report/importer/csv_importer.py
+++ inventory_report/importer/csv_importer.py
@@ -5,5 +5,7 @@ import csv
 class CsvImporter(Importer):
     @classmethod
     def import_data(cls, path: str):
+        if not path.__contains__(".csv"):
+            raise ValueError("Arquivo inválido")
         with open(path) as file:
             return list(csv.DictReader(file, delimiter=",", quotechar='"'))
--- inventory_report/importer/json_importer.py
+++ inventory_report/importer/json_importer.py
@@ -5,5 +5,7 @@ import json
 class JsonImporter(Importer):
     @classmethod
     def import_data(cls, path: str):
+        if not path.__contains__(".json"):
+            raise ValueError("Arquivo inválido")
         with open(path) as file:
             return list(json.load(file))
