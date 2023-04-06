249b43374a0a0d9eb338788c9b2f8e7bfdda5cac
Renan Fernandes <renzinestuods@gmail.com>
2023-04-06 17:34:00 -0300

    :sparkles: feat: Implementa a função main

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,5 +1,21 @@
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+import sys
+
+
 def main():
-    pass
+    try:
+        _, path, type = sys.argv
 
+        if path.__contains__(".csv"):
+            Importer = InventoryRefactor(CsvImporter)
+        elif path.__contains__(".json"):
+            Importer = InventoryRefactor(JsonImporter)
+        else:
+            Importer = InventoryRefactor(XmlImporter)
 
-# init
+        sys.stdout.write(Importer.import_data(path, type))
+    except ValueError:
+        sys.stderr.write("Verifique os argumentos\n")
